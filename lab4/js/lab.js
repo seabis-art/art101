b286a004f98d3694e91031d8b4e7c15325e4126d
sebastian delgado <serdelga@ucsc.edu>
2022-10-13 15:42:48 -0700

    lab 4

---
+++ lab4/js/lab.js
@@ -0,0 +1,18 @@
+// Summary. (use period)
+// Description. (use period)
+// @link   URL
+// @file   This files defines the MyClass class.
+// @author AuthorName.
+// @since  x.x.x
+//Task 1 Pseudocode for cooking a chicken breast:
+  //Make sure you have all the tools and ingredients (Chicken breast, seasonings of your choice (ex: salt and pepper), pan, oven, knife (optional), olive oil (or some oil of your choice), meat mallet (preferred), and a cutting board of some sort.
+    //Ensure the chicken breast is fully thawed (the best method is to leave it in the fridge for at least 24 hours).
+    //Evenly flatten chicken breast with a meat mallet on your cutting board.
+//Apply a generous amount of salt and a couple of pinches of pepper to both sides.
+//Allow chicken to sit at room temperature for 20 minutes.
+//Preheat the pan for 2 minutes over medium heat.
+//Apply 2 tbs of olive oil to the pan, making sure to cover the pan fully.
+//Let the oil heat up for 30 seconds.
+//Apply chicken breasts to the pan, and let it sit for 4 minutes.
+//Flip the chicken breast, and let it sit for 3 minutes.
+//Let the chicken breast sit for 5 minutes before cutting into it, and enjoy.
