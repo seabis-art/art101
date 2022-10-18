1d9102dc5c51ac823d23a86115d2de7452258e99
sebastian delgado <serdelga@ucsc.edu>
2022-10-17 22:01:24 -0700

    lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,18 @@
+//Note, that that is WRITELN with an "L" not WRITEIN
+document.writeln("Make: " + make + "<br>");
+
+//Define Variables
+make="Toyota";
+model="CRV";
+color="gray";
+year= "2007";
+
+// Calculate
+age= 2022-year;
+
+// Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "years<br>");
