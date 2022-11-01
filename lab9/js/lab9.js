b88c06261e85ce5e00e685ee8c7be9a44ae88548
sebastian delgado <serdelga@ucsc.edu>
2022-10-31 21:24:07 -0700

    lab 9

---
+++ lab9/js/lab9.js
@@ -0,0 +1,34 @@
+/**
+ * Author: Sebastian Delgado
+ * Created:   10.31.2022
+ *
+ * (c) Copyright by Blub Corp.
+ **/
+
+// label each element
+ document.getElementById("orange");
+null
+
+var outputEl = document.getElementById("orange");
+undefined
+
+// create elements
+var newEl = document.createElement("div");
+undefined
+
+var newEl = document.createElement("p");
+undefined
+
+var new1El = document.createElement("son");
+undefined
+
+var new2El = document.createElement("dad");
+undefined
+
+// run append child function
+bioEl.appendChild(new2El);
+new1El
+<son>​</son>​
+
+newEl.appendChild(new2El);
+<dad></dad>
