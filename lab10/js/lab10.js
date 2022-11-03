85762202caa74b3ff314046e6fd90b8ee9064f67
sebastian delgado <serdelga@ucsc.edu>
2022-11-02 22:24:45 -0700

    lab 10

---
+++ lab10/js/lab10.js
@@ -0,0 +1,29 @@
+/**
+ * Author: Sebastian Delgado
+ * Created:   11.2.2022
+ *
+ * (c) Copyright by Blub Corp.
+ **/
+
+ function sortUserName() {
+    var userName = window.prompt("Tell me your name");
+    console.log("userName =", userName);
+    // split the array
+    var nameArray = userName.split('');
+    console.log("nameArray =", nameArray);
+    // sort the array
+    var nameArraySort = nameArray.sort();
+    console.log("nameArraySort =", nameArraySort);
+    // join back to a string
+    var nameSorted = nameArraySort.join('');
+    console.log("nameSorted =", nameSorted);
+    //  userName.toLower().split("").sort().join("")
+    return nameSorted;
+
+    // adding button
+    var button = document.getElementById('my-button');
+    button.addEventListener('click', function() {
+
+    inputValue = document.getElementById('my-input').value;
+    console.log("Results", inputValue);
+});
